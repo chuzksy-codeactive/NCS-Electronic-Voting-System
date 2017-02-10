7b017101adfcea0d3e54ed12b402a5c1626c1ee6
Chuzksy <chuzksy@yahoo.com>
2017-02-10 14:18:39 +0100

    Added a class, used for sending email notifications

---
+++ VotingSystem/Models/SendNotifications.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace VotingSystem.Models
+{
+    public static class SendNotifications
+    {
+        public static string SendEmail(string email)
+        {
+            
+        }
+    }
+}
