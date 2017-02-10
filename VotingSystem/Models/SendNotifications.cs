c7e1729d0527e51af32ecf02a311017d1b6efd95
Chuzksy <chuzksy@yahoo.com>
2017-02-10 14:45:47 +0100

    Sending email code completed

--- VotingSystem/Models/SendNotifications.cs
+++ VotingSystem/Models/SendNotifications.cs
@@ -1,13 +1,48 @@
 ﻿using System.Net;
 using System.Net.Mail;
+using System.Text;
 
 namespace VotingSystem.Models
 {
-    public static class SendNotifications
+    public class SendNotifications
     {
-        public static string SendEmail(string email)
+        private NetworkCredential login;
+        private SmtpClient client;
+        private MailMessage msg;
+        string message = string.Empty;
+        public string SendEmail(string email)
         {
-            
+            login = new NetworkCredential("ncsprojectx", "password001");
+            client = new SmtpClient("stmp.gmail.com");
+            client.Port = 587;
+            client.EnableSsl = true;
+            client.Credentials = login;
+            msg = new MailMessage {From = new MailAddress("chuzksy@gmail.com", "Chuzksy Solutions", Encoding.UTF8)};
+            msg.Subject = "Voters Registration PIN";
+            msg.BodyEncoding = Encoding.UTF8;
+            msg.IsBodyHtml = true;
+            msg.Priority = MailPriority.Normal;
+            msg.DeliveryNotificationOptions = DeliveryNotificationOptions.OnFailure;
+            client.SendCompleted += Client_SendCompleted;
+            const string userstate = "Sending...";
+            client.SendAsync(msg, userstate);
+            return message;
+        }
+
+        private void Client_SendCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
+                message = "Sending email cancelled " + e.UserState;
+            }
+            if (e.Error != null)
+            {
+                message = e.UserState + " " + e.Error;
+            }
+            else
+            {
+                message = "Your email has been sent successfully";
+            }
         }
     }
 }
